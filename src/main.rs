b18389cd4e9bd7600dfb5222b21c9ae3d3b85203
Alex <mashkal2000@gmail.com>
2023-02-26 16:30:32 -0800

    Initial implementation.
    Prints full paths to every `*fetch` file in the PATH.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::env;
+use std::path::Path;
+use std::fs;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let paths = env::var("PATH").expect("PATH is undefined.");
+    for path in paths.split(':') {
+        let dir = Path::new(path);
+        let entries_result = fs::read_dir(dir);
+        match entries_result {
+            Ok(entries) => {
+                let fetches = entries
+                    .filter_map(|x| x.ok())
+                    .filter(|entry| entry.file_type().is_ok() && entry.file_type().unwrap().is_file())
+                    .filter_map(|entry| {
+                        let name = entry.file_name();
+                        let name_str = name.to_str();
+                        if name_str.is_some() && name_str.unwrap().ends_with("fetch") {
+                            Some(entry.path())
+                        } else {
+                            None
+                        }
+                    });
+                for fetch in fetches {
+                    println!("{}", fetch.display());
+                }
+            },
+            Err(err) => {
+                eprintln!("Failed to read path {} : {}", dir.display(), err);
+                exit(1);
+            }
+        };
+    }
 }
