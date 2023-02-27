ad0135d05f966af1bbdfafd6158d39d19ebcde11
Alex <mashkal2000@gmail.com>
2023-02-26 16:46:28 -0800

    Print only file name. Also remove duplicate entries since they cannot be distinguished by the file path - just the name.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::env;
 use std::path::Path;
 use std::fs;
@@ -5,31 +6,32 @@ use std::process::exit;
 
 fn main() {
     let paths = env::var("PATH").expect("PATH is undefined.");
+    let mut found_fetches: HashSet<String> = HashSet::new();
     for path in paths.split(':') {
         let dir = Path::new(path);
         let entries_result = fs::read_dir(dir);
         match entries_result {
             Ok(entries) => {
-                let fetches = entries
+                found_fetches.extend(entries
                     .filter_map(|x| x.ok())
                     .filter(|entry| entry.file_type().is_ok() && entry.file_type().unwrap().is_file())
                     .filter_map(|entry| {
                         let name = entry.file_name();
                         let name_str = name.to_str();
                         if name_str.is_some() && name_str.unwrap().ends_with("fetch") {
-                            Some(entry.path())
+                            Some(String::from(name_str.unwrap()))
                         } else {
                             None
                         }
-                    });
-                for fetch in fetches {
-                    println!("{}", fetch.display());
-                }
+                    }));
             },
             Err(err) => {
                 eprintln!("Failed to read path {} : {}", dir.display(), err);
                 exit(1);
             }
         };
+    }
+    for fetch in found_fetches {
+        println!("{}", fetch);
     }
 }
