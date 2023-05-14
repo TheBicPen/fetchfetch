4f670e743cff8ffbb511bf16a232becfa95674f4
Alex <mashkal2000@gmail.com>
2023-05-13 17:49:59 -0700

    Add: Check bash history for fetch execution

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::env::VarError;
+use std::io::BufRead;
 use std::{env, io};
-use std::path::Path;
-use std::fs;
+use std::path::{Path};
+use std::fs::{self, File};
+
+use dirs::home_dir;
 
 fn find_fetches(paths: Vec<String>) -> Result<HashSet<String>, io::Error> {
     let mut found_fetches: HashSet<String> = HashSet::new();
@@ -33,6 +36,27 @@ fn find_fetches(paths: Vec<String>) -> Result<HashSet<String>, io::Error> {
     Ok(found_fetches)
 }
 
+fn get_fetch_history() -> Result<HashMap<String, i32>, io::Error> {
+    let home_dir_path = home_dir().ok_or(
+        io::Error::new(io::ErrorKind::NotFound, "Unable to find home directory")
+    );
+    let bash_history_path = home_dir_path?.join(".bash_history");
+    let history_file = File::open(bash_history_path)?;
+    let mut history_fetch : HashMap<String, i32> = HashMap::new();
+    for maybe_line in io::BufReader::new(history_file).lines() {
+        let line = maybe_line?;
+        let words: Vec<&str> = line.split_whitespace().collect();
+        if words.len() > 0 {
+            if words[0].ends_with("fetch") {
+                history_fetch.entry(words[0].to_string())
+                    .and_modify(|x| *x += 1 )
+                    .or_insert(1);
+            }
+        }
+    }
+    Ok(history_fetch)
+}
+
 fn get_paths() -> Result<Vec<String>, VarError> {
     let paths = env::var("PATH")?;
     Ok(paths.split(":").map(str::to_owned).collect())
@@ -44,4 +68,9 @@ fn main() {
     for fetch in found_fetches {
         println!("{}", fetch);
     }
+    let fetch_history = get_fetch_history().expect("failed to read fetch history");
+    for (name, count) in fetch_history {
+        println!("{} {}", name, count);
+    }
+
 }
